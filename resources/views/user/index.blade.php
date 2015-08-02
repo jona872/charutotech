2e53803ad4b99dca8b2caff24aabf9a25eee67ed
jona872 <jona_872@hotmail.com>
2015-08-01 22:11:36 -0300

    first commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,34 @@
+@extends('blog')
+
+@section('content')
+
+</br>
+</br>
+<div class="container">
+<table class="table">
+        <thead>
+            <th>algo</th>
+            <th>algo</th>
+            <th>algo</th>
+            <th>algo</th>
+            <th> - </th>
+        </thead>
+        @foreach ($users as $u)
+        <tbody>
+            <td>{{$u->id}}</td>
+            <td>{{$u->name}}</td>
+            <td>{{$u->username}}</td>
+            <td>{{$u->password}}</td>
+            <td>
+            
+            
+            <!-- ya me hace de una la indexacion del us con su id corresp, y eso se lo paso a usuario.editar -->
+        </td>
+        </tbody>
+        @endforeach
+
+
+    </table>
+</div>
+
+@endsection
